b5dbd8f87eb07bbf07c1de2dd0a006aff8d5dd29
shafidark <67519541+shafidark@users.noreply.github.com>
2021-01-19 10:59:48 +0600

    shafi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 function App() {
+    const data = new Date()
     return (
         <div className="App">
             <form
@@ -10,23 +11,14 @@ function App() {
                 {/* Hidden fields: */}
                 <input type="hidden" name="EPS_MERCHANT" value="ABC0010"/>
                 <input type="hidden" name="EPS_TXNTYPE" value="0"/>
-                <input type="hidden" name="EPS_REFERENCEID" value="Test Reference"/>
+               
                 <input type="hidden" name="EPS_AMOUNT" value="1.00"/>
-                <input type="hidden" name="EPS_TIMESTAMP" value="201106141010"/>
-                <input
-                    type="hidden"
-                    name="EPS_FINGERPRINT"
-                    value="01a1edbb159aa01b99740508d79620251c2f871d"/>
+                <input type="hidden" name="EPS_TIMESTAMP" value={data}/>
+                <input type="hidden" name="EPS_FINGERPRINT"  value="01a1edbb159aa01b99740508d79620251c2f871d"/>
                 <input type="hidden" name="EPS_RESULTURL" value="https://www.resulturl.com"/>
                 <input type="hidden" name="EPS_REFERENCEID" value="Invoice#642193"/>
-                <input type="hidden" name="EPS_TXNTYPE" value="1"/>
-                <input type="hidden" name="EPS_AMOUNT" value="53.20"/>
-                <input type="hidden" name="EPS_TIMESTAMP" value="20110620123505"/>
-                <input type="hidden"
-                    name="EPS_FINGERPRINT"
-                    value="dae18a7c6b223472c7a7c8116acbdbf1170a033c89f3b9b8f940069c453436de"/> 
-{/* Customer-entered fields: */}
 
+                {/* Customer-entered fields: */}
                 <input type="text" name="EPS_CARDNUMBER"/>
                 <input type="text" name="EPS_EXPIRYMONTH"/>
                 <input type="text" name="EPS_EXPIRYYEAR"/>
