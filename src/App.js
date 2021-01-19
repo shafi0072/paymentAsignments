8cc9de7de311aa5917d4784b2daa1ee0191017a7
shafidark <67519541+shafidark@users.noreply.github.com>
2021-01-19 12:06:28 +0600

    shafi

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 
 function App() {
-    const data = new Date()
-    const time = data.getTime()
+    function pad2(n) { return n < 10 ? '0' + n : n }
+
+var date = new Date();
+
+const data =  date.getFullYear().toString() + pad2(date.getMonth() + 1) + pad2( date.getDate()) + pad2( date.getHours() ) + pad2( date.getMinutes() ) + pad2( date.getSeconds() );
     return (
         <div className="App">
             <form method="POST" action="https://test.api.securepay.com.au/directpost/authorise">
 
                 {/* Hidden fields: */}
-                <input type="hidden" name="EPS_MERCHANT" value="ABC0010"/>
+                <input type="hidden" name="EPS_MERCHANT" value="ABC0001"/>
                 <input type="hidden" name="EPS_TXNTYPE" value="0"/>
                
                 <input type="hidden" name="EPS_AMOUNT" value="1.00"/>
-                <input type="hidden" name="EPS_TIMESTAMP" value={time}/>
+                <input type="hidden" name="EPS_TIMESTAMP" value={data}/>
                 <input type="hidden" name="EPS_FINGERPRINT"  value="01a1edbb159aa01b99740508d79620251c2f871d"/>
                 <input type="hidden" name="EPS_RESULTURL" value="https://www.resulturl.com"/>
                 <input type="hidden" name="EPS_REFERENCEID" value="Invoice#642193"/>
